82c22a726ee5f57dc7e907004e1efbb1e6a0d8af
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-02-24 13:17:34 +0400

    login register role done making product routes

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,12 +1,19 @@
-// App.js
-
-import HomePage from "./components/HomePage";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import "./App.css"
+import Register from './components/Register';
 
 function App() {
-  <>
-    <HomePage/>
-  </>;
+  return (
+    <Router>
+      <Routes> 
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
